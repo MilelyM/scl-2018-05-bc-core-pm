c24bacd5301ae2bd7953a362e3785f07022f13b3
milelym <milelymarcial@gmail.com>
2018-05-31 21:24:39 -0400

    codigo cifra y descifra

--- src/cipher.js
+++ src/cipher.js
@@ -20,16 +20,31 @@ window.cipher = {
           // validar dentro den rango del abc
          
          // se tomo el id de htmt para crear una variable y concatenarla al codigo encriptado trasformado en letras
-          Resulcode = String.fromCharCode(AsciiCode);
+          Resulcode += String.fromCharCode(AsciiCode);
         // document.writeln(root);
     }
     // envia el resultado al id en htlm
 return Resulcode
     //replace remplaza algo dentro del string,Crea un objeto 'expresión regular' para encontrar el espacio, "g"busqueda global
     
-  }/*,
-  decode: () => {
+  },
+  decode: (texto,clave) => {
+    var AsciiCode= 0;
+    var Resulcode= "";
+      
+       
+        for( let index = 0; index < texto.length;index++){
     
-  }*/
+         
+            var AsciiCode = texto.charCodeAt(index);
+            AsciiCode= (AsciiCode+65-clave) % 26+65;
+     
+         
+          Resulcode += String.fromCharCode(AsciiCode);
+    }
+    return Resulcode
+  }
+
+
 }
 
--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ function cifrarClick(){
     document.getElementById("resultado").innerHTML = window.cipher.encode(texto,clave);
   
 }
+function cifrarClick2(){
+    var texto = document.getElementById("caja1").value;
+    var clave = parseInt(document.getElementById("caja2").value);
+    document.getElementById("resultado").innerHTML = window.cipher.decode(texto,clave);
+  
+}
 
  
